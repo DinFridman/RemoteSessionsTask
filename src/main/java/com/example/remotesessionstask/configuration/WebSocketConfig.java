4c7f56766a44e01621ea0dabeff7d350d64a680c
DinFridman <102311214+DinFridman@users.noreply.github.com>
2023-11-29 23:06:42 +0200

    Improved Latency

--- src/main/java/com/example/remotesessionstask/configuration/WebSocketConfig.java
+++ src/main/java/com/example/remotesessionstask/configuration/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/codeblocks-websocket").setAllowedOrigins(CLIENT_URL);
+        registry.addEndpoint("/codeblocks-websocket").setAllowedOrigins("*");
 
     }
 
