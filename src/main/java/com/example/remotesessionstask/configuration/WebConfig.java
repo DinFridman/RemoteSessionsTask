af553356ca811233c4ee0e58669ca9ad66855ec8
DinFridman <102311214+DinFridman@users.noreply.github.com>
2023-11-29 18:34:46 +0200

    frontend build added

--- src/main/java/com/example/remotesessionstask/configuration/WebConfig.java
+++ src/main/java/com/example/remotesessionstask/configuration/WebConfig.java
@@ -12,9 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://master--lambent-biscotti-10133a.netlify.app/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
+
     }
 }
--- src/main/java/com/example/remotesessionstask/controller/WebSocketController.java
+++ src/main/java/com/example/remotesessionstask/controller/WebSocketController.java
@@ -12,9 +12,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.Objects;
 import static com.example.remotesessionstask.utils.Constants.CODE_UPDATE_TYPE;
 import static com.example.remotesessionstask.utils.Constants.REMOTE_SESSION_TERMINATION_TYPE;
@@ -59,10 +56,7 @@ public class WebSocketController {
     }
 
 
-    @GetMapping("/")
-    public String index() {
-        return "test";
-    }
+
 
 }
 
