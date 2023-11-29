bfdcab2f7a807c9ae1a8f08d172cb4a40163926d
DinFridman <102311214+DinFridman@users.noreply.github.com>
2023-11-29 20:47:38 +0200

    testcommit

--- src/main/java/com/example/remotesessionstask/configuration/WebConfig.java
+++ src/main/java/com/example/remotesessionstask/configuration/WebConfig.java
@@ -6,15 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://master--lambent-biscotti-10133a.netlify.app/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 }
