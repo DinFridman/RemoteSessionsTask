c6e5093fcfdd73661661fb427087e6c348a5c0c0
DinFridman <102311214+DinFridman@users.noreply.github.com>
2023-12-04 11:43:40 +0200

    Update WebSocketController.java

--- src/main/java/com/example/remotesessionstask/controller/WebSocketController.java
+++ src/main/java/com/example/remotesessionstask/controller/WebSocketController.java
@@ -12,8 +12,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Objects;
 import static com.example.remotesessionstask.utils.Constants.CODE_UPDATE_TYPE;
