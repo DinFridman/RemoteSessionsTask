ba3e3c7445b12338ebe88831d552dcdfaba3ea79
DinFridman <102311214+DinFridman@users.noreply.github.com>
2023-12-05 15:38:47 +0200

    server port changed

--- src/main/java/com/example/remotesessionstask/utils/Constants.java
+++ src/main/java/com/example/remotesessionstask/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.example.remotesessionstask.utils;
 
 public class Constants {
-    public static final String CLIENT_URL = "https://remote-sessions-task.netlify.app";
+    public static final String CLIENT_URL = "http://localhost:3000";
     public static final String MENTOR_ROLE = "MENTOR";
     public static final String STUDENT_ROLE = "STUDENT";
     public static final boolean REMOTE_SESSION_TERMINATION_SIGNAL = true;
